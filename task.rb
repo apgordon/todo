127a6985838592b15838cee184b022d73d472cb1
apgordon <gordon.ap@gmail.com>
2017-10-21 12:50:49 -0400

    show all method (working)

--- task.rb
+++ task.rb
@@ -17,9 +17,18 @@ class Task
 		puts "#{index}: #{@@tasks[index].name}" 
 	end
 
+	def self.show_all
+		@@tasks.each_with_index do |i, index|
+			puts "Index: #{index}, Name: #{@@tasks[index].name}"
+		end
+	end
+
+
 end
 
 Task.create("buy milk")
 Task.show_one(0)
 Task.create("take out trash")
 Task.show_one(1)
+puts "\n\n"
+Task.show_all
